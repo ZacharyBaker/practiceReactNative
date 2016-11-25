54c59e575b3ef028a87df6c87a0411fb903ba741
Zach Baker <zach.r.baker1@gmail.com>
2016-11-25 13:21:51 -0800

    toying around

--- practiceNative/TestComponent.js
+++ practiceNative/TestComponent.js
@@ -1,16 +1,14 @@
 'use strict';
 
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Stylesheet } from 'react-native';
 
 export default class TestComponent extends Component {
 	render() {
 		return (
-			<View>
 				<Text>
 					Hey I am text. and a test. i am two tings.
 				</Text>
-			</View>
 		)
 	}
 }
